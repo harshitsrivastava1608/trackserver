7b7d4652ddefdc0ab689c51d7176e73d59a0b277
harshitsrivastava1608 <harshitsrivastava1608@gmail.com>
2025-04-18 04:39:46 +0530

    fix: enaled cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express= require('express')
+const cors = require('cors')
 const router = require('./routes/milksession.route')
 
 const PORT=process.env.PORT||7000
 
 const app=express()
+app.use(cors())
 app.use(express.json())
 app.use(router)
 app.get('/',(req,res)=>{
