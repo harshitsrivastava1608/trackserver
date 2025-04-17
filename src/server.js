824716082bf611b629d266198d25fac59e1e866d
harshitsrivastava1608 <harshitsrivastava1608@gmail.com>
2025-04-18 03:45:22 +0530

    feature initialized with express server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express= require('express')
+const router = require('./routes/milksession.route')
+
+const PORT=process.env.PORT||7000
+
+const app=express()
+app.use(express.json())
+app.use(router)
+app.get('/',(req,res)=>{
+    res.send({'msg':"Data Fetched"})
+})
+app.listen(PORT,()=>{
+    console.log('server up @',PORT)
+})
